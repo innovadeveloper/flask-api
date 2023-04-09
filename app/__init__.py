c391786c970883164523f26ec964422c3b3097af
kenny <kenny@192.168.1.157>
2023-04-08 21:27:32 -0500

    add app.route endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,12 @@ from flask import Flask
 from app import main
 from app.main.database import db, migration
 from app.main.logging import LOGGING_CONFIG
-from app.main.api import api
 
 # Flask App Initialization
 app = Flask(__name__)
 # configuration = main.settings[os.environ.get('APPLICATION_ENV', 'default')]
 app.config.from_object(main.settings[os.environ.get('APPLICATION_ENV', 'default')])
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-
 
 # Logs Initialization
 console = logging.getLogger('console')
@@ -24,4 +22,7 @@ db.init_app(app)
 migration.init_app(app, db)
 
 # Flask API Initialization
+from app.main.api import api
 api.init_app(app)
+# startRoutes(app)
+
--- app/main/api.py
+++ app/main/api.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from app.controllers.user import UserList, User
 from app.main.errors import errors
+from app import app
 
 # Flask API Configuration
 api = Api(
@@ -12,3 +13,13 @@ api = Api(
 api.add_resource(UserList, '/users')
 api.add_resource(User, '/users/<int:id>/')
 
+
+
+# def startRoutes(app):
+#     @app.route('/guide', methods=["GET"])
+#     def add_guide():
+#         return "hola mundo 3"
+    
+@app.route('/guide', methods=["GET"])
+def add_guide():
+    return "hola mundo 4"
